d11c1bae5d71c1b2df30e6b66c6cf5f28846d28a
sNickers86 <nick.weemaes.nw@gmail.com>
2025-06-06 14:52:44 +0200

    gallerij actief maar nog geen afbeeldingen

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,8 +1,10 @@
 import { initAccordion } from './animations/accordion.js';
 import { initNavbarToggle } from './animations/nav.js';
+import { initGallerySlideshows } from './animations/slideShow.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   initAccordion();
   initNavbarToggle();
+  initGallerySlideshows();
 });
 
